7d68fc1185945ff7696e851a9f165d61b8343788
pstamatop <pstamatop@gmail.com>
2024-03-20 21:06:24 +0200

    Fix verifier admin panel

--- src/verifier/verifierPanelRouter.ts
+++ src/verifier/verifierPanelRouter.ts
@@ -5,13 +5,11 @@ import AppDataSource from "../AppDataSource";
 import { VerifiablePresentationEntity } from "../entities/VerifiablePresentation.entity";
 import { appContainer } from "../services/inversify.config";
 import { TYPES } from "../services/types";
-import { VerifierConfigurationInterface } from "../services/interfaces";
+import { OpenidForPresentationsReceivingInterface, VerifierConfigurationInterface } from "../services/interfaces";
 import base64url from "base64url";
 import locale from "../configuration/locale";
-import { VerifierConfigurationService } from "../configuration/verifier/VerifierConfigurationService";
-import { PresentationSubmission } from "@wwwallet/ssi-sdk";
-import { JSONPath } from "jsonpath-plus";
 
+const openidForPresentationReceivingService = appContainer.get<OpenidForPresentationsReceivingInterface>(TYPES.OpenidForPresentationsReceivingService);
 
 
 const verifierPanelRouter = Router();
@@ -35,37 +33,35 @@ verifierPanelRouter.get('/', async (req, res) => {
 	})
 })
 
+type VerifiablePresentationWithDetails = VerifiablePresentationEntity & { holderInfo?: string, claims?: any };
 
 verifierPanelRouter.get('/filter/by/definition/:definition_id', async (req, res) => {
 	const definition_id = req.params.definition_id;
 	if (!definition_id) {
 		return res.status(500).send({ error: "No definition id was specified" });
 	}
-	const verifiablePresentations = await verifiablePresentationRepository.createQueryBuilder('vp')
+	let verifiablePresentations = await verifiablePresentationRepository.createQueryBuilder('vp')
 		.where("vp.presentation_definition_id = :definition_id", { definition_id: definition_id })
 		.getMany();
+
+	const presentationsWithDetails: VerifiablePresentationWithDetails[] = verifiablePresentations.map(vp => {
+		try {
+			const decoded = vp.raw_presentation ? JSON.parse(base64url.decode(vp.raw_presentation.split('.')[1])) : null as any;
+			const holderInfo = decoded?.vp?.holder || "No Holder Info";
+			const claims = vp.claims;
+			return { ...vp, holderInfo, claims } as VerifiablePresentationWithDetails;
+		} catch (error) {
+			console.error("Error decoding VP:", error);
+			return { ...vp, holderInfo: 'Error decoding holder info' } as VerifiablePresentationWithDetails;
+		}
+	});
+
 	return res.render('verifier/presentations.pug', {
 		lang: req.lang,
-		verifiablePresentations: verifiablePresentations,
+		verifiablePresentations: presentationsWithDetails,
 		locale: locale[req.lang]
 	})
 })
-
-
-
-type PresentationView = {
-	descriptor_mapped_value: {
-		inputDescriptorId: string;
-		vcView: VerifiableCredentialView;
-	}[]
-}
-
-type VerifiableCredentialView = {
-	rows: {
-		name: string;
-		value: string;
-	}[];
-}
 
 
 verifierPanelRouter.get('/presentation/:presentation_id', async (req, res) => {
@@ -73,58 +69,27 @@ verifierPanelRouter.get('/presentation/:presentation_id', async (req, res) => {
 	if (!presentation_id) {
 		return res.status(500).send({ error: "No presentation_id was specified" });
 	}
-	const verifiablePresentation = await verifiablePresentationRepository.createQueryBuilder('vp')
-		.where("vp.id = :presentation_id", { presentation_id: presentation_id })
-		.getOne();
-	
-	if (!verifiablePresentation || !verifiablePresentation.raw_presentation) {
-		return res.status(400).render('error', {
-			msg: "Verifiable presentation not found",
+	const { presentationClaims, rawPresentation } = await openidForPresentationReceivingService.getPresentationById(presentation_id as string);
+
+	if (!presentationClaims || !rawPresentation) {
+		return res.render('error.pug', {
+			msg: "Failed to get presentation",
+			code: 0,
 			lang: req.lang,
-			locale: locale[req.lang]
-		});
+			locale: locale[req.lang],
+		})
 	}
 
-	const presentationSubmission = verifiablePresentation.presentation_submission as PresentationSubmission;
+	const presentationPayload = JSON.parse(base64url.decode(rawPresentation.split('.')[1])) as any;
+	const credentials = presentationPayload.vp.verifiableCredential.map((vcString: any) => {
+		return JSON.parse(base64url.decode(vcString.split('.')[1]));
+	}).map((credential: any) => credential.vc);
 
-	const payload = JSON.parse(base64url.decode(verifiablePresentation.raw_presentation?.split('.')[1])) as any;
-	const vcList = payload.vp.verifiableCredential as string[];
-	const credentialList = [];
-	for (const vc of vcList) {
-		const vcPayload = JSON.parse(base64url.decode(vc.split('.')[1])) as any;
-		credentialList.push(vcPayload.vc);
-	}
-
-	const presentationDefinition = appContainer.resolve(VerifierConfigurationService)
-		.getPresentationDefinitions()
-		.filter(pd => 
-			pd.id == verifiablePresentation.presentation_definition_id
-		)[0];
-
-	let view: PresentationView = { descriptor_mapped_value: [] };
-	for (const descriptor of presentationDefinition.input_descriptors) {
-		const correspondingElementOnSubmission = presentationSubmission.descriptor_map.filter(desc => desc.id == descriptor.id)[0];
-
-		const vcRows = descriptor.constraints.fields.map((field) => {
-			const vcPath = correspondingElementOnSubmission.path;
-			const payload = JSON.parse(base64url.decode(verifiablePresentation.raw_presentation?.split('.')[1] as string)) as any;
-			const vcJwtString = JSONPath({ json: payload.vp, path: vcPath })[0];
-			const vcPayload = JSON.parse(base64url.decode(vcJwtString.split('.')[1])) as any;
-			const valuePath = field.path[0];
-			const value = JSONPath({ json: vcPayload.vc, path: valuePath })[0] as string;
-			return { name: valuePath, value: value }
-		});
-		view.descriptor_mapped_value.push({
-			inputDescriptorId: descriptor.id,
-			vcView: { rows: vcRows }
-		});
-	}
-	console.dir(view, { depth: null })
 	return res.render('verifier/detailed-presentation.pug', {
-		view: view,
-		status: verifiablePresentation.status,
 		lang: req.lang,
-		locale: locale[req.lang]
+		presentationClaims: presentationClaims,
+		credentialPayloads: credentials,
+		locale: locale[req.lang],
 	})
 })
 
