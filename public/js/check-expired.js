8ab3d74ada8494497f3623c4ded246d42e83ec5f
kkmanos <koukoularisman@gmail.com>
2024-06-19 16:34:57 +0300

    added revocation status handling

--- public/js/check-expired.js
+++ public/js/check-expired.js
@@ -2,13 +2,28 @@ function updateLayoutForExpiredCredentials(credentialPayloads) {
 	const currentDate = new Date();
 	credentialPayloads.forEach((credential, index) => {
 		const expirationDate = new Date(credential.expirationDate);
-		if (expirationDate < currentDate) {
-			const credentialBox = document.querySelectorAll('.credential-box')[index];
-			credentialBox.classList.add('expired-credential');
-			const expiredLabel = document.createElement('div');
-			expiredLabel.className = 'expired-label';
-			expiredLabel.textContent = 'Expired';
-			credentialBox.appendChild(expiredLabel);
-		}
+		const [crlURL, id] = credential.credentialStatus.id.split('#');
+		fetch(crlURL).then((response) => {
+			return response.json();
+		}).then(({ crl }) => {
+			console.log("CRL = ", crl);
+			const record = crl.filter((rec) => rec.id == id)[0];
+			if (record.revocation_date != null) { // is revoked
+				const credentialBox = document.querySelectorAll('.credential-box')[index];
+				credentialBox.classList.add('revoked-credential');
+				const revokedLabel = document.createElement('div');
+				revokedLabel.className = 'revoked-label';
+				revokedLabel.textContent = 'Revoked';
+				credentialBox.appendChild(revokedLabel);
+			}
+			else if (expirationDate < currentDate) {
+				const credentialBox = document.querySelectorAll('.credential-box')[index];
+				credentialBox.classList.add('expired-credential');
+				const expiredLabel = document.createElement('div');
+				expiredLabel.className = 'expired-label';
+				expiredLabel.textContent = 'Expired';
+				credentialBox.appendChild(expiredLabel);
+			}
+		})
 	});
 }
