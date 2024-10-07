a352c0f0541772ddbab943d54883db2dcd6c0227
kkmanos <koukoularisman@gmail.com>
2024-10-07 13:57:52 +0300

    added VCDMSupportedCredentialProtocol

--- src/lib/CredentialIssuerConfig/SupportedCredentialProtocol.ts
+++ src/lib/CredentialIssuerConfig/SupportedCredentialProtocol.ts
@@ -1,7 +1,7 @@
 import { JWK } from "jose";
 import { CredentialView } from "../../authorization/types";
 import { AuthorizationServerState } from "../../entities/AuthorizationServerState.entity";
-import { VerifiableCredentialFormat, Display } from "../../types/oid4vci";
+import { VerifiableCredentialFormat } from "../../types/oid4vci";
 import { CredentialSigner } from "../../services/interfaces";
 import { Request } from "express";
 
@@ -13,12 +13,19 @@ export interface SupportedCredentialProtocol {
 	getId(): string;
 	getFormat(): VerifiableCredentialFormat;
 	getTypes(): string[];
-	getDisplay(): Display;
+	getDisplay(): any;
 
 	getProfile(userSession: AuthorizationServerState): Promise<CredentialView | null>;
 	generateCredentialResponse(userSession: AuthorizationServerState, credentialRequest: Request, holderJWK: JWK): Promise<{ format?: VerifiableCredentialFormat, credential?: any, acceptance_token?: string }>;
-
-
 	
 	exportCredentialSupportedObject(): any;
+}
+
+
+export interface VCDMSupportedCredentialProtocol extends SupportedCredentialProtocol {
+	/**
+	 * VCDM draft spec https://github.com/danielfett/sd-jwt-vc-dm?tab=readme-ov-file#sd-jwt-vc-dm-credential-format
+	 * @returns  an object according to https://vcstuff.github.io/sd-jwt-vc-types/draft-fett-oauth-sd-jwt-vc-types.html
+	 */
+	metadata(): any;
 }
