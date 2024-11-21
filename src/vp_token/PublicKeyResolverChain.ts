9a8ee595526db1f5a4256f04de4af2d3263af82f
kkmanos <koukoularisman@gmail.com>
2024-11-21 18:35:44 +0200

    introduction of PresentationParserChain and PublicKeyResolverChain increase flexibility

---
+++ src/vp_token/PublicKeyResolverChain.ts
@@ -0,0 +1,26 @@
+import { KeyLike } from "jose";
+import { IPublicKeyResolver } from "./IPublicKeyResolver";
+import { sdJwtPublicKeyResolverUsingX5CHeader } from "./sdJwtPublicKeyResolverUsingX5CHeader";
+
+export class PublicKeyResolverChain {
+
+	constructor(private resolverList: IPublicKeyResolver[] = [
+		sdJwtPublicKeyResolverUsingX5CHeader
+	]) { }
+
+	addResolver(p: IPublicKeyResolver): this {
+		this.resolverList.push(p);
+		return this;
+	}
+
+	async resolve(rawPresentation: any, format: string): Promise<{ publicKey: KeyLike, isTrusted: boolean } | { error: "UNABLE_TO_RESOLVE_PUBKEY" }> {
+		for (const p of this.resolverList) {
+			const result = await p.resolve(rawPresentation, format);
+			if ('error' in result) {
+				continue;
+			}
+			return { ...result };
+		}
+		return { error: "UNABLE_TO_RESOLVE_PUBKEY" };
+	}
+}
