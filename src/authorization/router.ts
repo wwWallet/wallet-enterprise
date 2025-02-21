22f1c02e705ebbf83d7628e84af0c3e2d4cffe46
kkmanos <koukoularisman@gmail.com>
2025-02-21 18:53:28 +0200

    registed all authentication components

--- src/authorization/router.ts
+++ src/authorization/router.ts
@@ -1,15 +1,26 @@
 import { Router } from "express";
 import { consent } from "./consentPage";
-import { authChain } from "../configuration/authentication/authenticationChain";
+import { credentialConfigurationRegistryService } from "../services/instances";
+import { credentialConfigurationRegistryServiceEmitter } from "../services/CredentialConfigurationRegistryService";
 
 const authorizationRouter = Router();
 
-authChain.components.map(c => {
-	authorizationRouter.use(async (req, res, next) => {
-		c.authenticate(req, res, next)
+function registerAuthChains() {
+	credentialConfigurationRegistryService.getAllRegisteredCredentialConfigurations().map((conf) => {
+		const authChain = conf.getAuthenticationChain();
+		authChain.components.map(c => {
+			console.log("Registering authentication component: ", c.identifier)
+			authorizationRouter.use(async (req, res, next) => {
+				c.authenticate(req, res, next)
+			});
+		})
 	});
-})
+}
 
-authorizationRouter.use('/consent', consent);
+credentialConfigurationRegistryServiceEmitter.on('initialized', () => {
+	registerAuthChains();
+	authorizationRouter.use('/consent', consent);
+});
+
 
 export { authorizationRouter };
