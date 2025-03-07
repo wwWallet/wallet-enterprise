77820269d43a56890ba4cd1943d076909918a6bd
kkmanos <koukoularisman@gmail.com>
2025-03-07 12:40:55 +0200

    fix image extraction

--- src/lib/initializeCredentialEngine.ts
+++ src/lib/initializeCredentialEngine.ts
@@ -1,6 +1,6 @@
 import { HttpClient, MsoMdocParser, MsoMdocVerifier, ParsingEngine, PublicKeyResolverEngine, SDJWTVCParser, SDJWTVCVerifier } from 'core';
 import { config } from '../../config';
-import axios, { AxiosRequestHeaders } from 'axios';
+import axios from 'axios';
 import { webcrypto } from "node:crypto";
 import { OpenID4VCICredentialRendering } from 'core/dist/functions/openID4VCICredentialRendering';
 import { CredentialRenderingService } from 'core/dist/rendering';
@@ -8,11 +8,11 @@ import { CredentialRenderingService } from 'core/dist/rendering';
 export function initializeCredentialEngine() {
 	console.log("Initializing credential engine...")
 	const httpClient: HttpClient = {
-		async get(url, headers) {
-			return axios.get(url, { headers: headers as AxiosRequestHeaders }).then((res) => (res?.data ? {...res.data} : {})).catch((err) => (err?.response?.data ? {...err.response.data} : { }));
+		async get(url, headers, opts) {
+			return axios.get(url, { ...opts, headers: headers as any }).then((res) => (res?.data ? { status: res.status, data: res.data, headers: res.headers } : {})).catch((err) => (err?.response?.data ? { ...err.response.data } : {}));
 		},
-		async post(url, data, headers) {
-			return axios.post(url, data, { headers: headers as AxiosRequestHeaders }).then((res) => (res?.data ? {...res.data} : {})).catch((err) => (err?.response?.data ? {...err.response.data} : { }));
+		async post(url, data, headers, opts) {
+			return axios.post(url, data, { ...opts, headers: headers as any }).then((res) => (res?.data ? { status: res.status, data: res.data, headers: res.headers } : {})).catch((err) => (err?.response?.data ? { ...err.response.data } : {}));
 		},
 	}
 	
