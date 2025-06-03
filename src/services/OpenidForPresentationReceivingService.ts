d06430bd8a2d1d7ebe2d4f493eafacab302edc9f
kkmanos <koukoularisman@gmail.com>
2025-06-03 17:29:11 +0300

    added backwards compatibility support for vc+sd-jwt

--- src/services/OpenidForPresentationReceivingService.ts
+++ src/services/OpenidForPresentationReceivingService.ts
@@ -342,7 +342,7 @@ export class OpenidForPresentationsReceivingService implements OpenidForPresenta
 				throw new Error(`Couldn't find vp_token for path ${path}`);
 			}
 			const vp_token = jsonPathResult[0];
-			if (desc.format == VerifiableCredentialFormat.DC_SDJWT) {
+			if (desc.format == VerifiableCredentialFormat.DC_SDJWT || desc.format == VerifiableCredentialFormat.VC_SDJWT) {
 				// const sdJwt = vp_token.split('~').slice(0, -1).join('~') + '~';
 				const input_descriptor = rpState!.presentation_definition!.input_descriptors.filter((input_desc: any) => input_desc.id == desc.id)[0];
 				if (!input_descriptor) {
